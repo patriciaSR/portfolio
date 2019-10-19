9ef97f4c63ece62b0e08c68d5482fcdfa6719e93
patriciaSR <patricia.suarez.psr@gmail.com>
2019-10-19 11:07:32 +0200

    add coming sonn text and change backgrouns styles and page title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <h1 className="portfolio__title">PORTFOLIO EN CONSTRUCCIÓN</h1>
         <p className="coming__text">Coming soon ...</p>
         <img className="Coming__gif" src={popcorns} alt="coming soon portfolio gif" />
