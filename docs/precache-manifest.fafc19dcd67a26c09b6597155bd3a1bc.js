2a1b66305eb5e3a5a4281622d664f17184939f4f
patriciaSR <patricia.suarez.psr@gmail.com>
2019-10-19 16:13:50 +0200

    create docs folder

---
+++ docs/precache-manifest.fafc19dcd67a26c09b6597155bd3a1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248c165e723f1ba137ddb3a69d2fd723",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de69d3a74bf28c486245",
+    "url": "./static/css/main.87daf31a.chunk.css"
+  },
+  {
+    "revision": "41372494385ec00dc13c",
+    "url": "./static/js/2.ad0b3095.chunk.js"
+  },
+  {
+    "revision": "de69d3a74bf28c486245",
+    "url": "./static/js/main.e586beba.chunk.js"
+  },
+  {
+    "revision": "d9fec5f6efaf04749bab",
+    "url": "./static/js/runtime-main.c6822cd3.js"
+  },
+  {
+    "revision": "0ad479263634432597a94ba63037a5a3",
+    "url": "./static/media/jp2.0ad47926.jpg"
+  }
+]);
